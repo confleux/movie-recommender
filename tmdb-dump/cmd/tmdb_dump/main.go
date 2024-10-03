8a3c3e9b61161c020707599897e25addab4d03af
confleux <confleux@gmail.com>
2024-10-03 18:28:53 +0300

    refactor: naming, add backoff

--- tmdb-dump/cmd/tmdb_dump/main.go
+++ tmdb-dump/cmd/tmdb_dump/main.go
@@ -11,34 +11,58 @@ import (
 	"tmdb-dump/internal/service"
 )
 
+const (
+	initialBackoff     = 100 * time.Millisecond
+	maxRequestRetries  = 5
+	backoffCoefficient = 1.5
+)
+
 func main() {
 	cfg := config.MustLoad()
 
-	MongoDB, err := mongodb.NewMongoDB(cfg.Mongo.Uri, cfg.Mongo.Database, cfg.Mongo.Collection)
+	mongoDB, err := mongodb.NewMongoDB(cfg.Mongo.Uri, cfg.Mongo.Database, cfg.Mongo.Collection)
 	if err != nil {
-		log.Fatalf("failed to create MongoDB: %w", err)
+		log.Fatalf("failed to create MongoDB: %v", err)
 	}
 
-	ApiClient := api_client.NewApiClient(cfg.TmdbApi.BaseUrl, cfg.TmdbApi.Token)
+	apiClient := api_client.NewApiClient(cfg.TmdbApi.BaseUrl, cfg.TmdbApi.Token)
 
-	MovieRepository := mongodb_movie.NewMovieRepository(MongoDB)
+	movieRepository := mongodb_movie.NewMovieRepository(mongoDB)
 
-	MovieService := service.NewMovieService(ApiClient)
+	movieService := service.NewMovieService(apiClient)
 
 	for page := 1; page < cfg.PagesCount; page++ {
-		select {
-		case <-time.After(2 * time.Second):
-			result, err := MovieService.FetchMovies(page)
-			if err != nil {
-				log.Fatalf("failed to fetch movies: %v", err)
-			}
+		retryCount := 0
+		backoff := initialBackoff
 
-			insertedId, err := MovieRepository.InsertMoviesPage(result)
-			if err != nil {
-				log.Fatalf("failed to insert movie page: %v", err)
-			}
+	backoffLoop:
+		for {
+			select {
+			case <-time.After(backoff):
+				result, err := movieService.FetchMovies(page)
+				if err != nil {
+					retryCount++
 
-			fmt.Printf("Successfully added %s (page: %d)", insertedId, page)
+					if retryCount > maxRequestRetries {
+						log.Fatalf("%v", err)
+					}
+
+					backoff = time.Duration(float64(backoff) * backoffCoefficient)
+					continue
+				}
+
+				retryCount = 0
+				backoff = initialBackoff
+
+				insertedId, err := movieRepository.InsertMoviesPage(result)
+				if err != nil {
+					log.Fatalf("failed to insert movie page: %v", err)
+				}
+
+				fmt.Printf("Successfully added %s (page: %d)\n", insertedId, page)
+
+				break backoffLoop
+			}
 		}
 	}
 }
