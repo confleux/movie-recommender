23300e65688476e9161457c0642b956b1094cb65
confleux <confleux@gmail.com>
2024-10-14 16:01:42 +0300

    chore: implement movie service

---
+++ tg-bot/internal/service/movie.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"tg-bot/internal/model"
+	"tg-bot/internal/repository"
+)
+
+type MovieService struct {
+	movieRepository *repository.MovieRepository
+}
+
+func NewMovieService(movieRepository *repository.MovieRepository) *MovieService {
+	return &MovieService{movieRepository: movieRepository}
+}
+
+func (ms *MovieService) GetRandomMovies(count int) ([]model.Movie, error) {
+	if movies, err := ms.movieRepository.GetRandomMovies(context.Background(), count); err != nil {
+		return nil, fmt.Errorf("get random movies: %w", err)
+	} else {
+		return movies, nil
+	}
+}
